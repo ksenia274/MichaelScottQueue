7cbc0ae344e0474b78fcf4ea04da0467b449d949
ksenia274 <kcen,yackubowa@2013@gmail.com>
2024-11-30 14:54:44 +0300

    1. Stress test

---
+++ src/test/kotlin/Test.kt
@@ -0,0 +1,31 @@
+import org.jetbrains.kotlinx.lincheck.annotations.*
+import org.jetbrains.kotlinx.lincheck.check
+import org.jetbrains.kotlinx.lincheck.strategy.managed.modelchecking.ModelCheckingOptions
+import org.jetbrains.kotlinx.lincheck.strategy.stress.StressOptions
+import kotlin.test.Test
+
+class MichaelScottQueueLincheckTest {
+    private val queue = MichaelScottQueue<Int>()
+
+    @Operation
+    fun enqueue(value: Int) {
+        queue.enqueue(value)
+    }
+
+    @Operation
+    fun dequeue(): Int? {
+        return queue.dequeue()
+    }
+
+    @Operation
+    fun isEmpty(): Boolean {
+        return queue.isEmpty()
+    }
+
+    @Test
+    fun stressTest() = StressOptions()
+        .iterations(100)
+        .invocationsPerIteration(1000)
+        .check(this::class)
+
+}
